d66ee7e1c4fae91ed4761b50bc3c1272fb7c06ab
Carsten Brandt <mail@cebe.cc>
2015-04-23 01:09:22 +0200

    Update build.sh

--- redis/build.sh
+++ redis/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "building docker for redis $REDIS_VERSION."
-sed -ri "s/^(ENV REDIS_VERSION).*/\1 $REDIS_VERSION/" Dockerfile
-docker build --rm=false -t yiitest/redis:$REDIS_VERSION .
-
+if (docker images | grep -P "yiitest/redis\s+$REDIS_VERSION"); then
+    echo "docker for redis $REDIS_VERSION already exists"
+else
+    echo "building docker for redis $REDIS_VERSION."
+    sed -ri "s/^(ENV REDIS_VERSION).*/\1 $REDIS_VERSION/" Dockerfile
+    docker build --rm=false -t yiitest/redis:$REDIS_VERSION .
+fi
