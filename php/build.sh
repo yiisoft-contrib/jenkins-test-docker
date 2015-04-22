c22eee08fe39e57004e7962004b2828572835ede
Carsten Brandt <mail@cebe.cc>
2015-04-23 01:08:38 +0200

    Update build.sh

--- php/build.sh
+++ php/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "building docker for php $PHP_VERSION."
-sed -ri "s/^(ENV PHP_VERSION).*/\1 $PHP_VERSION/" Dockerfile
-docker build --rm=false -t yiitest/php:$PHP_VERSION .
-
+if (docker images | grep -P "yiitest/php\s+$PHP_VERSION"); then
+    echo "docker for php $PHP_VERSION already exists"
+else
+    echo "building docker for php $PHP_VERSION."
+    sed -ri "s/^(ENV PHP_VERSION).*/\1 $PHP_VERSION/" Dockerfile
+    docker build --rm=false -t yiitest/php:$PHP_VERSION .
+fi
